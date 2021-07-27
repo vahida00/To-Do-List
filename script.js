8b085554df42b3afeebaf864519b80c321741bc2
vahida00 <vahidamohammad00@gmail.com>
2021-07-27 07:37:41 +0530

    update filter and checkbox

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var form=document.getElementById("addForm");
 var itemList=document.getElementById("items");
 var userInput=document.getElementById('item');
+var filter=document.getElementById('filter');
 form.addEventListener("submit",addItem);
 itemList.addEventListener("click",removeItem);
 itemList.addEventListener("click",strikeItem);
+filter.addEventListener('keyup',filterItems);
 function addItem(e){
     e.preventDefault(); /*to avoid Normal submission*/
     if(userInput.value!=='' && userInput.value!==undefined){
@@ -11,13 +13,13 @@ function addItem(e){
       var a=document.createElement('a');
       a.className="list-group-item list-group-item-action";
       a.id="listItem";
-      var checkBox=document.createElement('input');
-      checkBox.type="checkbox";
-      checkBox.className="strike";
-      checkBox.id="close";
-      a.appendChild(checkBox);
       var aText=document.createTextNode(newItem);
       a.appendChild(aText);
+      var checkBox=document.createElement('input');
+      checkBox.type="checkbox";
+      checkBox.className="strike float-right ml-2 checkbox-lg";
+      checkBox.id="close";
+      a.appendChild(checkBox);
       var deleteBtn=document.createElement("button");
       deleteBtn.className="btn btn-danger btn-sm float-right delete";
       deleteBtn.appendChild(document.createTextNode('X'));
@@ -43,3 +45,20 @@ function strikeItem(e){
     a.style.textDecorationLine="line-through";
   }
 }
+
+function filterItems(e){
+	var text=e.target.value.toLowerCase();
+	//  var items=document.getElementsByTagName('a');
+  var items=itemList.getElementsByTagName('a');
+   console.log(items);
+	 Array.from(items).forEach(function(item){
+	 	var itemName = item.firstChild.textContent;
+		 console.log(itemName);
+	 	if(itemName.toLowerCase().indexOf(text) != -1){
+	 		item.style.display = "block";
+	 	}
+	 	else{
+	 		item.style.display ="none";
+	 	}
+	})
+} 
